0af1499bf76e4278ec837e2ae45d61b41e382c47
Simon Xiang <simonxiang@zohomail.com>
2020-08-05 21:45:46 -0500

    problem 4, will finish later

---
+++ C/problem4/config.h
@@ -0,0 +1,6 @@
+/* Variable names */
+static double digit = 3;
+static double c1;
+static double c2;
+static double to;
+static unsigned int max;
---
+++ C/problem4/problem4.c
@@ -0,0 +1,15 @@
+/* Problem 4 */
+/* Largest palindome that's the product of two three digit numbers */
+/* Compile with gcc problem4.c -lm on a unix based system */
+#include <stdio.h>
+#include <math.h>
+#include "config.h"
+
+int main () {
+    c1 = pow(10, digit) - 1;
+    c2 = pow(10, digit) - 1;
+    to = pow(10, digit - 1) - 1;
+    printf("c1: %g c2: %g to: %g \n", c1, c2, to);
+    
+    return 0;
+}
